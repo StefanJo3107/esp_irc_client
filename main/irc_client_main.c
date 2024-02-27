abe36811cebad6e9cc4a4ebb42fc20c2d6a4e1df
StefanJo3107 <sjovanovic0831@gmail.com>
2024-02-27 02:30:09 +0100

    Project template

---
+++ main/irc_client_main.c
@@ -0,0 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+}
